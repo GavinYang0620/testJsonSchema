9323ddda1f17ef550c940552642ca89a54d2add7
GavinYang0620 <131344074+GavinYang0620@users.noreply.github.com>
2023-04-20 16:23:03 +0800

    Update TestServiceController.java

--- api/booking/service/TestServiceController.java
+++ api/booking/service/TestServiceController.java
@@ -14,39 +14,39 @@ import com.shipmentonline.msb1bk.api.booking.to.test.UnLocationLocation;
 @CrossOrigin({ "http://plusdvp.shipmentonline.com" })
 @RestController
 public class TestServiceController implements TestService {
-	@Override
-	public ResponseEntity<Object> anyOf(String str) {
-		ObjectMapper mapper = new ObjectMapper();
-		ObjectNode result = mapper.createObjectNode();
-		try {
-			mapper.registerSubtypes(AddressLocation.class);
-			mapper.registerSubtypes(UnLocationLocation.class);
-			
-			PlaceOfBLIssue pob = mapper.readValue(str, PlaceOfBLIssue.class);
-			result.putPOJO("placeOfBLIssue", pob);
-			
-			return new ResponseEntity<Object>(result, HttpStatus.OK);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.BAD_REQUEST);
-		}
-	}
+    @Override
+    public ResponseEntity<Object> anyOf(String str) {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode result = mapper.createObjectNode();
+        try {
+            mapper.registerSubtypes(AddressLocation.class);
+            mapper.registerSubtypes(UnLocationLocation.class);
+            
+            PlaceOfBLIssue pob = mapper.readValue(str, PlaceOfBLIssue.class);
+            result.putPOJO("placeOfBLIssue", pob);
+            
+            return new ResponseEntity<Object>(result, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<Object>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 
-	@Override
-	public ResponseEntity<Object> oneOf(String str) {
-		ObjectMapper mapper = new ObjectMapper();
-		ObjectNode result = mapper.createObjectNode();
-		try {
-			mapper.registerSubtypes(AddressLocation.class);
-			mapper.registerSubtypes(UnLocationLocation.class);
-			
-			PlaceOfBLIssue pob = mapper.readValue(str, PlaceOfBLIssue.class);
-			result.putPOJO("placeOfBLIssue", pob);
-			
-			return new ResponseEntity<Object>(result, HttpStatus.OK);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.BAD_REQUEST);
-		}
-	}
+    @Override
+    public ResponseEntity<Object> oneOf(String str) {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode result = mapper.createObjectNode();
+        try {
+            mapper.registerSubtypes(AddressLocation.class);
+            mapper.registerSubtypes(UnLocationLocation.class);
+            
+            PlaceOfBLIssue pob = mapper.readValue(str, PlaceOfBLIssue.class);
+            result.putPOJO("placeOfBLIssue", pob);
+            
+            return new ResponseEntity<Object>(result, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<Object>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
