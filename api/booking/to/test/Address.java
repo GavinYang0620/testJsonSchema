29d27cbdea3f06ec1aade873d48e4ec7389e347b
GavinYang0620 <131344074+GavinYang0620@users.noreply.github.com>
2023-04-20 16:23:30 +0800

    Update Address.java

--- api/booking/to/test/Address.java
+++ api/booking/to/test/Address.java
@@ -5,76 +5,76 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Address {
-	@JsonProperty("name")
-	String name;
-	
-	@JsonProperty("street")
-	String street;
-	
-	@JsonProperty("streetNumber")
-	String streetNumber;
-	
-	@JsonProperty("floor")
-	String floor;
-	
-	@JsonProperty("postCode")
-	String postCode;
-	
-	@JsonProperty("city")
-	String city;
-	
-	@JsonProperty("stateRegion")
-	String stateRegion;
-	
-	@JsonProperty("country")
-	String country;
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getStreet() {
-		return street;
-	}
-	public void setStreet(String street) {
-		this.street = street;
-	}
-	public String getStreetNumber() {
-		return streetNumber;
-	}
-	public void setStreetNumber(String streetNumber) {
-		this.streetNumber = streetNumber;
-	}
-	public String getFloor() {
-		return floor;
-	}
-	public void setFloor(String floor) {
-		this.floor = floor;
-	}
-	public String getPostCode() {
-		return postCode;
-	}
-	public void setPostCode(String postCode) {
-		this.postCode = postCode;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getStateRegion() {
-		return stateRegion;
-	}
-	public void setStateRegion(String stateRegion) {
-		this.stateRegion = stateRegion;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
+    @JsonProperty("name")
+    String name;
+    
+    @JsonProperty("street")
+    String street;
+    
+    @JsonProperty("streetNumber")
+    String streetNumber;
+    
+    @JsonProperty("floor")
+    String floor;
+    
+    @JsonProperty("postCode")
+    String postCode;
+    
+    @JsonProperty("city")
+    String city;
+    
+    @JsonProperty("stateRegion")
+    String stateRegion;
+    
+    @JsonProperty("country")
+    String country;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getStreet() {
+        return street;
+    }
+    public void setStreet(String street) {
+        this.street = street;
+    }
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+    public String getFloor() {
+        return floor;
+    }
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
+    public String getPostCode() {
+        return postCode;
+    }
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getStateRegion() {
+        return stateRegion;
+    }
+    public void setStateRegion(String stateRegion) {
+        this.stateRegion = stateRegion;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
