774f0a6d3026d28e65cfee940711c319a669719c
GavinYang0620 <131344074+GavinYang0620@users.noreply.github.com>
2023-04-20 16:23:50 +0800

    Update AddressLocation.java

--- api/booking/to/test/AddressLocation.java
+++ api/booking/to/test/AddressLocation.java
@@ -10,26 +10,26 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonAutoDetect(fieldVisibility=JsonAutoDetect.Visibility.ANY, getterVisibility=JsonAutoDetect.Visibility.NONE)
 public class AddressLocation implements PlaceOfBLIssue {
-	public String locationName;
-	
-	@NotBlank
-	public Address address;
-	
-	@Schema
-	public String getLocationName() {
-		return locationName;
-	}
-	
-	public void setLocationName(String locationName) {
-		this.locationName = locationName;
-	}
-	
-	@Schema
-	public Address getAddress() {
-		return address;
-	}
+    public String locationName;
+    
+    @NotBlank
+    public Address address;
+    
+    @Schema
+    public String getLocationName() {
+        return locationName;
+    }
+    
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+    
+    @Schema
+    public Address getAddress() {
+        return address;
+    }
 
-	public void setAddress(Address address) {
-		this.address = address;
-	}
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
